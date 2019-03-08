8b13218b3ec264d1dfe66d8c84ac1d67715f051d
Namita <aroranamita33@gmail.com>
2019-03-08 11:54:49 +0530

    comp

---
+++ java/ReverseWords.java
@@ -0,0 +1,18 @@
+package com.stackroute;
+class ReverseWords {
+
+    String wordRev(String str){
+        StringBuilder sb = new StringBuilder();
+        sb.append(str);
+        sb = sb.reverse();
+        str = sb.toString();
+        String arrStr[] = str.split(" ");
+        int i;
+        String newStr = "";
+        for (i=arrStr.length-1; i>=0; i--){
+            newStr = newStr + arrStr[i] + " ";
+        }
+        return newStr.trim();
+    }
+
+}
